5ac5a51f2825f6f3c72637cb61ae3d8efcabfc0b
AKLAVYA <112847832+AKLAVYA12@users.noreply.github.com>
2025-04-18 21:39:51 +0530

    css and task js

--- backend/dashboard.js
+++ backend/dashboard.js
@@ -36,5 +36,79 @@ document.addEventListener('DOMContentLoaded', async () => {
    sessionStorage.clear();
   });
 });
-
 window.addEventListener('load', configureClient);
+
+const darkModeBtn = document.getElementById("toggleDarkMode");
+
+let theme = "dark";
+
+darkModeBtn.addEventListener("click", function () {
+  if(theme === "dark"){
+
+  document.body.classList.remove("dark-mode");  
+  darkModeBtn.textContent = "☀️";
+  theme = "bright";
+
+  } else{
+    document.body.classList.add("dark-mode");
+    darkModeBtn.textContent = "🌙";
+    theme = "dark";
+  }
+});
+
+
+document.getElementById("addTaskBtn").addEventListener('click',()=>{
+ 
+  
+  const taskInput = document.getElementById("taskInput").value;
+  const taskText = taskInput.trim();
+  
+  if (taskText === "") {
+    window.alert("please enter a task");
+    return;
+  } else {
+  
+  const alltask = document.querySelectorAll(".task-text");
+  const counter = alltask.length + 1;  
+
+  const todoList = document.getElementById("todoList");
+
+  const newTask = document.createElement('li');
+  if (todoList.style.display === "none") {
+    todoList.style.display = "inline";
+  }
+  
+  newTask.className = "todo-item";
+
+  newTask.innerHTML = `
+    <div class="task-info">
+      <span class="task-text" >${counter}. ${taskText}</span>
+    </div>
+    <div class="task-actions">
+      <button class="edit-btn" title="Edit Task">✎</button>
+      <button class="delete-btn" title="Delete Task">🗑️</button>
+    </div>
+  `;
+
+    todoList.appendChild(newTask);
+
+  
+    function renumb() {
+      const alltext = document.querySelectorAll(".task-text");
+      alltext.forEach((thistext,index ) =>{
+        const text = thistext.textContent;
+        const taxtonly = text.split(". ").slice(1).join(". ");
+        thistext.textContent = `${index}. ${taxtonly}`;
+      }) 
+    }
+
+    newTask.querySelector(".delete-btn").addEventListener('click',() =>{
+      if (confirm("Are you sure you want to delete this task?")) {
+        newTask.remove();
+        renumb();
+      }
+    });
+  
+  }
+});
+
